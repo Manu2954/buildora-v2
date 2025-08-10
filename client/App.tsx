823c455b3aa092b443eece147669edc52f919b69
ManojKumar Vaduka <vadukamanojkumar@gmail.com>
2025-08-11 04:29:47 +0530

    Add Buildora Enterprise CTA landing page

--- client/App.tsx
+++ client/App.tsx
@@ -4,7 +4,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { SidebarProvider } from "./contexts/SidebarContext";
 import Index from "./pages/Index";
@@ -15,9 +15,34 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Search from "./pages/Search";
 import CTA from "./pages/CTA";
+import EnterpriseCTA from "./pages/EnterpriseCTA";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
+
+function AppRoutes() {
+  const location = useLocation();
+  const hideNav = location.pathname === "/enterprise";
+
+  return (
+    <SidebarProvider>
+      {!hideNav && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/cta" element={<CTA />} />
+        <Route path="/enterprise" element={<EnterpriseCTA />} />
+        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </SidebarProvider>
+  );
+}
 
 export default function App() {
   return (
@@ -26,21 +51,7 @@ export default function App() {
         <Toaster />
         <Sonner />
         <BrowserRouter>
-          <SidebarProvider>
-            <Navbar />
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/services" element={<Services />} />
-              <Route path="/orders" element={<Orders />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/search" element={<Search />} />
-              <Route path="/cta" element={<CTA />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </SidebarProvider>
+          <AppRoutes />
         </BrowserRouter>
       </TooltipProvider>
     </QueryClientProvider>

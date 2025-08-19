fe8d37ceb27835f12734bead7bca72e9491f927f
ManojKumar Vaduka <vadukamanojkumar@gmail.com>
2025-08-20 01:48:55 +0530

    feat: reuse CTA UI for dedicated app

--- apps/cta/client/App.tsx
+++ apps/cta/client/App.tsx
@@ -6,8 +6,9 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import CTA from "./pages/CTA";
 import NotFound from "./pages/NotFound";
+import { SidebarProvider } from "@/contexts/SidebarContext";
 
 const queryClient = new QueryClient();
 
@@ -17,11 +18,13 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <SidebarProvider>
+          <Routes>
+            <Route path="/" element={<CTA />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </SidebarProvider>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
